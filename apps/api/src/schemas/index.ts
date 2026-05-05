9e26dd57ecd75dd52e6e9b7b5fbd7bd07d2f1d07
elainees-devs <elainees.devs@gmail.com>
2026-05-05 16:59:18 +0300

    feat(validation): export centralized Joi schemas for all core entities

---
+++ apps/api/src/schemas/index.ts
@@ -0,0 +1,7 @@
+export {schoolSchema} from "./school.schema";
+export {classSchema} from "./class.schema";
+export {subjectSchema} from "./subject.schema";
+export {studentSchema} from "./student.schema";
+export {assessmentSchema} from "./assessment.schema";
+export {classSubjectSchema} from "./class-subject.schema";
+export {userSchema, authenticateUserSchema} from "./user.schema";
