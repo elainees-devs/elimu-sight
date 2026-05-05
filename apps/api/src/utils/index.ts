b1ef26bff1f2c62c8645442a3c23b40bcb1aad5b
elainees-devs <elainees.devs@gmail.com>
2026-05-05 18:35:26 +0300

    refactor: centralize exports for constants and JWT utilities

--- apps/api/src/utils/index.ts
+++ apps/api/src/utils/index.ts
@@ -1,1 +1,5 @@
+// constants
 export {subscriptionPlans,roles, insightTypes, generatedBy, genders, examTypes} from "./constants";
+
+// jwt
+export {generateToken, verifyToken, refreshToken} from "./jwt";
