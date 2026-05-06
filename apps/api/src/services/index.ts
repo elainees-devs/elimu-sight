bf98269584d6b5af16781df1a3c1cbe424ae53ce
elainees-devs <elainees.devs@gmail.com>
2026-05-06 20:36:08 +0300

    chore(services): export AssessmentService, StudentService, and InsightService from index

--- apps/api/src/services/index.ts
+++ apps/api/src/services/index.ts
@@ -5,3 +5,6 @@ export { ClassService } from "./class.service";
 export { SubjectService } from "./subject.service";
 export {ClassSubjectService} from "./class-subject.service";
 export {AssessmentService} from "./assessment.service";
+export {StudentService} from "./student.service";
+export {InsightService} from "./insight.service";
+
