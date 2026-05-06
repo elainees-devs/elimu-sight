8e993cc1af47ce830b4ea1ca3e4f90c411f89e3f
elainees-devs <elainees.devs@gmail.com>
2026-05-06 20:36:59 +0300

    feat(insight): add InsightAnalyticsService structure for dashboard aggregation queries

---
+++ apps/api/src/services/insights/insight.analytics.service.ts
@@ -0,0 +1,23 @@
+
+export class InsightAnalyticsService {
+    // For Dashboard Aggregration in Frontend
+// ===============================
+// GET INSIGHTS BY CLASS LOGIC
+// ===============================
+
+// ===============================
+// GET INSIGHTS BY STUDENT LOGIC
+// ===============================
+
+// ===============================
+// GET INSIGHTS BY SUBJECT LOGIC
+// ===============================
+
+// ===============================
+// GET INSIGHTS BY TYPE LOGIC
+// ===============================
+
+// ===============================
+// GET INSIGHTS BY PERIOD LOGIC
+// ===============================
+}
