cae8ef088e2d50a9fb4b36a29b4ca317345a1030
elainees-devs <elainees.devs@gmail.com>
2026-05-06 19:36:11 +0300

    chore(services): export ClassSubjectService and AssessmentService from index

--- apps/api/src/services/index.ts
+++ apps/api/src/services/index.ts
@@ -3,3 +3,5 @@ export { AuthService } from "./auth.service";
 export { UserService } from "./user.service";
 export { ClassService } from "./class.service";
 export { SubjectService } from "./subject.service";
+export {ClassSubjectService} from "./class-subject.service";
+export {AssessmentService} from "./assessment.service";
