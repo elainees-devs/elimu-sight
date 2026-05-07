118f44abb419f1d86d874e8852cf7aa6d84d713b
elainees-devs <elainees.devs@gmail.com>
2026-05-07 15:31:32 +0300

    chore: export ClassSubject, Student, and Assessment controllers

--- apps/api/src/controllers/index.ts
+++ apps/api/src/controllers/index.ts
@@ -2,3 +2,6 @@ export {AuthController} from "./auth.controller";
 export {SchoolController} from "./school.controller";
 export {ClassController} from "./class.controller";
 export {SubjectController} from "./subject.controller";
+export {ClassSubjectController} from "./class-subject.controller";
+export {StudentController} from "./student.controller";
+export {AssessmentController} from "./assessment.controller";
