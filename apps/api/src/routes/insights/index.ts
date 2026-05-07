ce0254380c7ed38df8edc20a5d76c4831ae8daf2
elainees-devs <elainees.devs@gmail.com>
2026-05-07 17:53:06 +0300

    chore(insights): add barrel exports for insight routes

---
+++ apps/api/src/routes/insights/index.ts
@@ -0,0 +1,3 @@
+export {default as insightCrudRoute} from "./insight.crud.route";
+export {default as insightQueryRoute} from "./insight.query.route";
+export {default as insightAnalyticsRoute} from "./insight.analytics.route";
