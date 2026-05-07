afe6b400d1315c04c13c379cdb4144c7c93396c8
elainees-devs <elainees.devs@gmail.com>
2026-05-07 17:57:18 +0300

    chore(insights): export InsightController from controller index

--- apps/api/src/controllers/index.ts
+++ apps/api/src/controllers/index.ts
@@ -6,3 +6,4 @@ export {SubjectController} from "./subject.controller";
 export {ClassSubjectController} from "./class-subject.controller";
 export {StudentController} from "./student.controller";
 export {AssessmentController} from "./assessment.controller";
+export {InsightController} from "./insight.controller";
