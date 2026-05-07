eb9d0c9449367a1426856daec8e7ed61597e735c
elainees-devs <elainees.devs@gmail.com>
2026-05-08 00:12:23 +0300

    refactor(mappers): centralize exports and add missing ID mappers across modules

--- apps/api/src/mappers/index.ts
+++ apps/api/src/mappers/index.ts
@@ -1,14 +1,16 @@
+import { toSubjectId } from "./subject.mapper";
+
 // school mappers
-export {SchoolDB,toSchoolResponse, toSchoolListResponse, toCreateSchoolDB, toUpdateSchoolDB} from "./school.mapper";
+export {SchoolDB,toSchoolResponse, toSchoolListResponse, toCreateSchoolDB, toUpdateSchoolDB, toSchoolId} from "./school.mapper";
 
 // user mappers
-export {toUserResponse,toUserListResponse ,toCreateUserDB, toUpdateUserDB} from "./user.mapper";
+export {toUserResponse,toUserListResponse ,toCreateUserDB, toUpdateUserDB, toUserId} from "./user.mapper";
 
 // subject mappers
-export {toSubjectResponse, toSubjectListResponse, toCreateSubjectDB, toUpdateSubjectDB, SubjectDB} from "./subject.mapper";
+export {toSubjectResponse, toSubjectListResponse, toCreateSubjectDB, toUpdateSubjectDB, SubjectDB, toSubjectId} from "./subject.mapper";
 
 // class mappers
-export {toClassResponse, toClassListResponse, toCreateClassDB, toUpdateClassDB} from "./class.mapper";
+export {toClassResponse, toClassListResponse, toCreateClassDB, toUpdateClassDB, toClassId} from "./class.mapper";
 
 // class-subject mappers
 export {ClassSubjectDB,toClassSubjectId,toClassSubjectResponse, toClassSubjectListResponse, toCreateClassSubjectDB, toUpdateClassSubjectDB} from "./class-subject.mapper";
@@ -20,4 +22,4 @@ export {StudentDB,toStudentResponse, toStudentListResponse, toCreateStudentDB, t
 export {AssessmentDB,toAssessmentResponse, toAssessmentListResponse, toCreateAssessmentDB, toUpdateAssessmentDB, toAssessmentId} from "./assessment.mappers";
 
 // insight mappers
-export {toInsightResponse, toInsightListResponse, toCreateInsightDB, toUpdateInsightDB} from "./insight.mapper";
+export {toInsightResponse, toInsightListResponse, toCreateInsightDB, toUpdateInsightDB, toInsightId} from "./insight.mapper";
