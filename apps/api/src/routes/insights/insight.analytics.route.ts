30edb18c3f92a86866ccb1871bf1c91602692d78
elainees-devs <elainees.devs@gmail.com>
2026-05-08 00:10:15 +0300

    refactor(insights): standardize router to inline controller pattern

--- apps/api/src/routes/insights/insight.analytics.route.ts
+++ apps/api/src/routes/insights/insight.analytics.route.ts
@@ -9,35 +9,40 @@ const controller = new InsightController();
 router.get(
   "/classes/:classId/insights",
   authenticateMiddleware,
-  (req, res, next) => controller.getInsightsByClass(req, res, next)
+  (req, res, next) =>
+    controller.getInsightsByClass(req, res, next),
 );
 
 // BY STUDENT
 router.get(
   "/students/:studentId/insights",
   authenticateMiddleware,
-  (req, res, next) => controller.getInsightsByStudent(req, res, next)
+  (req, res, next) =>
+    controller.getInsightsByStudent(req, res, next),
 );
 
-// BY SUBJECT
+/// BY SUBJECT
 router.get(
   "/subjects/:subjectId/insights",
   authenticateMiddleware,
-  (req, res, next) => controller.getInsightsBySubject(req, res, next)
+  (req, res, next) =>
+    controller.getInsightsBySubject(req, res, next),
 );
 
 // BY TYPE
 router.get(
   "/insights/type/:type",
   authenticateMiddleware,
-  (req, res, next) => controller.getInsightsByType(req, res, next)
+  (req, res, next) =>
+    controller.getInsightsByType(req, res, next),
 );
 
 // BY PERIOD
 router.get(
   "/insights/period/:period",
   authenticateMiddleware,
-  (req, res, next) => controller.getInsightsByPeriod(req, res, next)
+  (req, res, next) =>
+    controller.getInsightsByPeriod(req, res, next),
 );
 
 export default router;
