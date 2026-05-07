ff23aea5ad9df582dc7d83ffbaef4e2ba1f3d81f
elainees-devs <elainees.devs@gmail.com>
2026-05-07 15:26:52 +0300

    chore: export AssessmentId param helper from params index

--- apps/api/src/utils/index.ts
+++ apps/api/src/utils/index.ts
@@ -1,17 +1,33 @@
 // constants
-export {subscriptionPlans,roles, insightTypes, generatedBy, genders, examTypes} from "./constants";
+export {
+  subscriptionPlans,
+  roles,
+  insightTypes,
+  generatedBy,
+  genders,
+  examTypes,
+} from "./constants";
 
 // jwt
-export {generateToken, verifyToken, refreshToken} from "./jwt";
+export { generateToken, verifyToken, refreshToken } from "./jwt";
 
 // prisma
-export {prisma} from "./prisma";
+export { prisma } from "./prisma";
 
 // app error
-export {ApiError} from "./app-error";
+export { ApiError } from "./app-error";
 
 // hash
-export {hashPassword, comparePassword} from "./hash";
+export { hashPassword, comparePassword } from "./hash";
 
 // params
-export {toIdParam, toSchoolIdParam, toClassIdParam, toSubjectIdParam, toStudentIdParam, toEmailParam, toNameParam} from "./params";
+export {
+  toIdParam,
+  toSchoolIdParam,
+  toClassIdParam,
+  toSubjectIdParam,
+  toStudentIdParam,
+  toEmailParam,
+  toNameParam,
+  toAssessmentIdParam,
+} from "./params";
