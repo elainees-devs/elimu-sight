1fdfa82ecf242a468f6b74342753dcfe48053c1d
elainees-devs <elainees.devs@gmail.com>
2026-05-07 15:52:30 +0300

    chore: export validateSchoolAccess middleware from index

--- apps/api/src/middlewares/index.ts
+++ apps/api/src/middlewares/index.ts
@@ -1,3 +1,4 @@
 export { errorHandler } from "./error.middleware";
 export { validate } from "./validate.middleware";
-export { authenticateMiddleware } from "./auth.middleware";   
+export { authenticateMiddleware } from "./auth.middleware"; 
+export { validateSchoolAccess } from "./validateSchoolAccess.middleware";  
