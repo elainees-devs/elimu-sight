c4f298c3fd1fb81002b3fb4dd5a84af61778dcaa
elainees-devs <elainees.devs@gmail.com>
2026-05-07 10:37:59 +0300

    feat: export class and subject controllers in index.ts

--- apps/api/src/controllers/index.ts
+++ apps/api/src/controllers/index.ts
@@ -1,2 +1,4 @@
 export {AuthController} from "./auth.controller";
 export {SchoolController} from "./school.controller";
+export {ClassController} from "./class.controller";
+export {SubjectController} from "./subject.controller";
