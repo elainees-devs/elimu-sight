887d001c98ae840c6ceb4a176c9195e8fecff3d3
elainees-devs <elainees.devs@gmail.com>
2026-05-07 16:44:29 +0300

    feat(routes): Add exports for school, auth, class, student, subject, user, class-subject, and assessment routers

---
+++ apps/api/src/routes/index.ts
@@ -0,0 +1,9 @@
+export { default as schoolRouter } from "./school.route";
+export { default as authRouter } from "./auth.route"
+export { default as classRouter } from "./class.route";
+export { default as studentRouter } from "./student.route";
+export {default as subjectRouter} from "./subject.route";
+export {default as userRouter} from "./user.route";
+export {default as classSubjectRouter} from "./class-subject.route";
+export {default as assessmentRouter} from "./assessment.route";
+
